a11d89aa259246cabf39139c98b884daba665a5b
Michael Lapadula <mlapadula@Michaels-MacBook-Pro.local>
2016-10-31 11:54:17 -0400

    Update Remember to not be static / singleton

--- remember/src/main/java/com/mlapadula/remember2/Remember.java
+++ remember/src/main/java/com/mlapadula/remember2/Remember.java
@@ -29,29 +29,22 @@ import java.util.concurrent.ConcurrentMap;
  */
 public class Remember {
 
-	private static final Remember INSTANCE = new Remember();
-
 	private static final String TAG = Remember.class.getSimpleName();
 
 	/**
 	 * Lock to ensure that only one disk write happens at a time.
 	 */
-	private static final Object SHARED_PREFS_LOCK = new Object();
-
-	/**
-	 * Flag that ensures this store is initialized before using.
-	 */
-	private volatile boolean mWasInitialized = false;
+	private final Object SHARED_PREFS_LOCK = new Object();
 
 	/**
 	 * The context to use.
 	 */
-	private volatile Context mAppContext;
+	private final Context mAppContext;
 
 	/**
 	 * The name of the shared preferences file to use.
 	 */
-	private static String mSharedPrefsName;
+	private final String mSharedPrefsName;
 
 	/**
 	 * Our data. This is a write-through cache of the data we're storing in SharedPreferences.
@@ -61,33 +54,22 @@ public class Remember {
 	/**
 	 * Constructor.
 	 */
-	private Remember() {
-		// Nothing to do here.
+	private Remember(Context context, String sharedPrefsName) {
+        long start = SystemClock.uptimeMillis();
+        mAppContext = context.getApplicationContext();
+        mSharedPrefsName = sharedPrefsName;
+
+        // Read from shared prefs
+        SharedPreferences prefs = getSharedPreferences();
+        mData = new ConcurrentHashMap<String,Object>();
+        mData.putAll(prefs.getAll());
+
+        long delta = SystemClock.uptimeMillis() - start;
+        Log.i(TAG, "Remember took " + delta + " ms to init");
 	}
 
 	/**
-	 * Initializes this store with the given context.
-	 */
-	private void initWithContext(Context context, String sharedPrefsName) {
-		// Time ourselves
-		long start = SystemClock.uptimeMillis();
-
-		// Set vars
-		mAppContext = context.getApplicationContext();
-		mSharedPrefsName = sharedPrefsName;
-
-		// Read from shared prefs
-		SharedPreferences prefs = getSharedPreferences();
-		mData = new ConcurrentHashMap<String,Object>();
-		mData.putAll(prefs.getAll());
-		mWasInitialized = true;
-
-		long delta = SystemClock.uptimeMillis() - start;
-		Log.i(TAG, "Remember took " + delta + " ms to init");
-	}
-
-	/**
-	 * Initializes the store.
+     * Creates a new instance of {@link Remember}
 	 *
 	 * @param context
 	 * 		the context to use. Using the application context is fine here.
@@ -95,30 +77,14 @@ public class Remember {
 	 * 		the name of the shared prefs file to use
 	 * @return the singleton instance that was initialized.
 	 */
-	public static synchronized Remember init(Context context, String sharedPrefsName) {
+	public static synchronized Remember create(Context context, String sharedPrefsName) {
 		// Defensive checks
 		if (context == null || TextUtils.isEmpty(sharedPrefsName)) {
 			throw new RuntimeException(
 					"You must provide a valid context and shared prefs name when initializing Remember");
 		}
 
-		// Initialize ourselves
-		if (!INSTANCE.mWasInitialized) {
-			INSTANCE.initWithContext(context, sharedPrefsName);
-		}
-
-		return INSTANCE;
-	}
-
-	/**
-	 * @return the singleton instance to use.
-	 */
-	private static Remember getInstance() {
-		if (!INSTANCE.mWasInitialized) {
-			throw new RuntimeException(
-					"Remember was not initialized! You must call Remember.init() before using this.");
-		}
-		return INSTANCE;
+        return new Remember(context, sharedPrefsName);
 	}
 
 	/**
@@ -205,8 +171,8 @@ public class Remember {
 	/**
 	 * Clears all data from this store.
 	 */
-	public static void clear() {
-		getInstance().clear(null);
+	public void clear() {
+		clear(null);
 	}
 
 	/**
@@ -216,13 +182,13 @@ public class Remember {
 	 * 		the callback to fire when done. The callback will be fired on the UI thread,
 	 * 		and will be passed 'true' if successful, 'false' if not.
 	 */
-	public static void clear(final Callback callback) {
-		getInstance().mData.clear();
+	public void clear(final Callback callback) {
+		mData.clear();
 		new AsyncTask<Void,Void,Boolean>() {
 			@Override
 			protected Boolean doInBackground(Void... params) {
 				synchronized (SHARED_PREFS_LOCK) {
-					SharedPreferences.Editor editor = getInstance().getSharedPreferences().edit();
+					SharedPreferences.Editor editor = getSharedPreferences().edit();
 					editor.clear();
 					return editor.commit();
 				}
@@ -240,8 +206,8 @@ public class Remember {
 	/**
 	 * Removes the mapping indicated by the given key.
 	 */
-	public static void remove(String key) {
-		getInstance().remove(key, null);
+	public void remove(String key) {
+		remove(key, null);
 	}
 
 	/**
@@ -251,13 +217,13 @@ public class Remember {
 	 * 		the callback to fire when done. The callback will be fired on the UI thread,
 	 * 		and will be passed 'true' if successful, 'false' if not.
 	 */
-	public static void remove(final String key, final Callback callback) {
-		getInstance().mData.remove(key);
+	public void remove(final String key, final Callback callback) {
+		mData.remove(key);
 		new AsyncTask<Void,Void,Boolean>() {
 			@Override
 			protected Boolean doInBackground(Void... params) {
 				synchronized (SHARED_PREFS_LOCK) {
-					SharedPreferences.Editor editor = getInstance().getSharedPreferences().edit();
+					SharedPreferences.Editor editor = getSharedPreferences().edit();
 					editor.remove(key);
 					return editor.commit();
 				}
@@ -275,36 +241,36 @@ public class Remember {
 	/**
 	 * Put a float. This saves to memory immediately and saves to disk asynchronously.
 	 */
-	public static Remember putFloat(final String key, final float value) {
-		return getInstance().saveAsync(key, value, null);
+	public Remember putFloat(final String key, final float value) {
+		return saveAsync(key, value, null);
 	}
 
 	/**
 	 * Put an int. This saves to memory immediately and saves to disk asynchronously.
 	 */
-	public static Remember putInt(String key, int value) {
-		return getInstance().saveAsync(key, value, null);
+	public Remember putInt(String key, int value) {
+		return saveAsync(key, value, null);
 	}
 
 	/**
 	 * Put a long. This saves to memory immediately and saves to disk asynchronously.
 	 */
-	public static Remember putLong(String key, long value) {
-		return getInstance().saveAsync(key, value, null);
+	public Remember putLong(String key, long value) {
+		return saveAsync(key, value, null);
 	}
 
 	/**
 	 * Put a String. This saves to memory immediately and saves to disk asynchronously.
 	 */
-	public static Remember putString(String key, String value) {
-		return getInstance().saveAsync(key, value, null);
+	public Remember putString(String key, String value) {
+		return saveAsync(key, value, null);
 	}
 
 	/**
 	 * Put a boolean. This saves to memory immediately and saves to disk asynchronously.
 	 */
-	public static Remember putBoolean(String key, boolean value) {
-		return getInstance().saveAsync(key, value, null);
+	public Remember putBoolean(String key, boolean value) {
+		return saveAsync(key, value, null);
 	}
 
 	/**
@@ -314,8 +280,8 @@ public class Remember {
 	 * 		the callback to fire when done. The callback will be fired on the UI thread,
 	 * 		and will be passed 'true' if successful, 'false' if not.
 	 */
-	public static Remember putFloat(final String key, final float value, final Callback callback) {
-		return getInstance().saveAsync(key, value, callback);
+	public Remember putFloat(final String key, final float value, final Callback callback) {
+		return saveAsync(key, value, callback);
 	}
 
 	/**
@@ -325,8 +291,8 @@ public class Remember {
 	 * 		the callback to fire when done. The callback will be fired on the UI thread,
 	 * 		and will be passed 'true' if successful, 'false' if not.
 	 */
-	public static Remember putInt(String key, int value, final Callback callback) {
-		return getInstance().saveAsync(key, value, callback);
+	public Remember putInt(String key, int value, final Callback callback) {
+		return saveAsync(key, value, callback);
 	}
 
 	/**
@@ -336,8 +302,8 @@ public class Remember {
 	 * 		the callback to fire when done. The callback will be fired on the UI thread,
 	 * 		and will be passed 'true' if successful, 'false' if not.
 	 */
-	public static Remember putLong(String key, long value, final Callback callback) {
-		return getInstance().saveAsync(key, value, callback);
+	public Remember putLong(String key, long value, final Callback callback) {
+		return saveAsync(key, value, callback);
 	}
 
 	/**
@@ -347,8 +313,8 @@ public class Remember {
 	 * 		the callback to fire when done. The callback will be fired on the UI thread,
 	 * 		and will be passed 'true' if successful, 'false' if not.
 	 */
-	public static Remember putString(String key, String value, final Callback callback) {
-		return getInstance().saveAsync(key, value, callback);
+	public Remember putString(String key, String value, final Callback callback) {
+		return saveAsync(key, value, callback);
 	}
 
 	/**
@@ -358,16 +324,16 @@ public class Remember {
 	 * 		the callback to fire when done. The callback will be fired on the UI thread,
 	 * 		and will be passed 'true' if successful, 'false' if not.
 	 */
-	public static Remember putBoolean(String key, boolean value, final Callback callback) {
-		return getInstance().saveAsync(key, value, callback);
+	public Remember putBoolean(String key, boolean value, final Callback callback) {
+		return saveAsync(key, value, callback);
 	}
 
 	/**
 	 * Gets a float with the given key. Defers to the fallback value if the mapping didn't exist, wasn't a float,
 	 * or was null.
 	 */
-	public static float getFloat(String key, float fallback) {
-		Float value = getInstance().get(key, Float.class);
+	public float getFloat(String key, float fallback) {
+		Float value = get(key, Float.class);
 		return value != null ? value : fallback;
 	}
 
@@ -375,8 +341,8 @@ public class Remember {
 	 * Gets an int with the given key. Defers to the fallback value if the mapping didn't exist, wasn't an int,
 	 * or was null.
 	 */
-	public static int getInt(String key, int fallback) {
-		Integer value = getInstance().get(key, Integer.class);
+	public int getInt(String key, int fallback) {
+		Integer value = get(key, Integer.class);
 		return value != null ? value : fallback;
 	}
 
@@ -384,8 +350,8 @@ public class Remember {
 	 * Gets a long with the given key. Defers to the fallback value if the mapping didn't exist, wasn't a long,
 	 * or was null.
 	 */
-	public static long getLong(String key, long fallback) {
-		Long value = getInstance().get(key, Long.class);
+	public long getLong(String key, long fallback) {
+		Long value = get(key, Long.class);
 		return value != null ? value : fallback;
 	}
 
@@ -393,8 +359,8 @@ public class Remember {
 	 * Gets a String with the given key. Defers to the fallback value if the mapping didn't exist, wasn't a String,
 	 * or was null.
 	 */
-	public static String getString(String key, String fallback) {
-		String value = getInstance().get(key, String.class);
+	public String getString(String key, String fallback) {
+		String value = get(key, String.class);
 		return value != null ? value : fallback;
 	}
 
@@ -402,8 +368,8 @@ public class Remember {
 	 * Gets a boolean with the given key. Defers to the fallback value if the mapping didn't exist, wasn't a boolean,
 	 * or was null.
 	 */
-	public static boolean getBoolean(String key, boolean fallback) {
-		Boolean value = getInstance().get(key, Boolean.class);
+	public boolean getBoolean(String key, boolean fallback) {
+		Boolean value = get(key, Boolean.class);
 		return value != null ? value : fallback;
 	}
 
@@ -412,8 +378,8 @@ public class Remember {
 	 * 
 	 * @return true if we have a mapping for the given key
 	 */
-	public static boolean containsKey(String key) {
-		return getInstance().mData.containsKey(key);
+	public boolean containsKey(String key) {
+		return mData.containsKey(key);
 	}
 
 	/**
