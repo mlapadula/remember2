bce4f81ee8a85a59cfdf78250a6a34ec66a046a8
Michael Lapadula <mlapadula@gmail.com>
2016-11-01 11:36:29 -0400

    Put more examples into the test app

--- sample-app/src/main/java/com/mlapadula/remembersample/RememberSample.java
+++ sample-app/src/main/java/com/mlapadula/remembersample/RememberSample.java
@@ -3,11 +3,16 @@ package com.mlapadula.remembersample;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.mlapadula.remember2.Remember;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * A simple activity that counts how many times it's been resumed via {@link Remember}.
@@ -44,6 +49,43 @@ public class RememberSample extends AppCompatActivity {
         textView.setText(testString);
 
         getRemember(this).putInt(KEY, howMany + 1);
+
+        // Some other simple examples:
+        getRemember(this).putFloat("test-float", 123.0f);
+        getRemember(this).putString("test-string", "hello world!");
+        getRemember(this).putBoolean("test-boolean", true);
+        getRemember(this).putLong("test-long", 54321L);
+
+        Log.d("Remember", "put float: " + getRemember(this).getFloat("test-float", 0f));
+        Log.d("Remember", "put string: " + getRemember(this).getString("test-string", ""));
+        Log.d("Remember", "put boolean: " + getRemember(this).getBoolean("test-boolean", false));
+        Log.d("Remember", "put long: " + getRemember(this).getLong("test-long", 0L));
+
+        // JSON object example, with callback:
+        JSONObject jsonObj = new JSONObject();
+        try {
+            jsonObj.put("some-json-key", "some-json-value");
+        } catch (JSONException err) {
+            // Don't care
+        }
+        getRemember(this).putJsonObject("test-json-object", jsonObj, new Remember.Callback() {
+            @Override
+            public void apply(Boolean success) {
+                Log.d("Remember", "put json object: " + getRemember(RememberSample.this).getJsonObject("test-json-object", null));
+            }
+        });
+
+        // JSON array example, with callback:
+        JSONArray jsonArr = new JSONArray();
+        jsonArr.put(1);
+        jsonArr.put(2);
+        jsonArr.put(3);
+        getRemember(this).putJsonArray("test-json-array", jsonArr, new Remember.Callback() {
+            @Override
+            public void apply(Boolean success) {
+                Log.d("Remember", "put json array: " + getRemember(RememberSample.this).getJsonArray("test-json-array", null));
+            }
+        });
     }
 
     @Override
